fc386a881fe18416fa71be5514ed1225bf0f7281
Sam Genduso <sam.genduso@gmail.com>
2015-07-15 13:40:08 -0600

    choose color and width of shape

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -1,31 +1,34 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 var shapeChoice = document.getElementById('shape-choice');
+var shapeWidth = document.getElementById('shape-width');
+var shapeColor = document.getElementById('color-choice');
 
 //draw square
 //fillRect(x, y, width, height)
-function drawSquare(x,y,w) {
-ctx.fillStyle = 'blue';
-ctx.fillRect(x-w/2, y-w/2, w, w);
+function drawSquare(x,y,w, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x-w/2, y-w/2, w, w);
 }
 
 //draw circle
 //arc(x, y, radius, startAngle, endAngle, anticlockwise);
-function drawCircle(x, y) {
-ctx.beginPath();
-ctx.fillStyle = 'green';
-ctx.arc(x, y, 50, 0, Math.PI*2, false);
-ctx.fill();
+function drawCircle(x, y, w, color) {
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.arc(x, y, w/2, 0, Math.PI*2, false);
+  ctx.fill();
 }
 
 function onClick(event) {
   var x = event.clientX - canvas.offsetLeft;
   var y = event.clientY - canvas.offsetTop;
-  // alert('you clicked at ' + x + ', ' + y);
-  if (shapeChoice.value === 'square') {
-    drawSquare(x,y,100);
+  var w = shapeWidth.value;
+  var color = shapeColor.value;
+    if (shapeChoice.value === 'square') {
+    drawSquare(x,y,w, color);
   } else {
-    drawCircle(x,y);
+    drawCircle(x,y,w, color);
   }
 }
 

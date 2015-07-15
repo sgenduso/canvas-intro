fa7c03987542f57d03307cb704f633ffc70ca10d
Sam Genduso <sam.genduso@gmail.com>
2015-07-15 14:40:11 -0600

    buttons for erase squares, erase circles

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -3,6 +3,10 @@ var ctx = canvas.getContext('2d');
 var shapeChoice = document.getElementById('shape-choice');
 var shapeWidth = document.getElementById('shape-width');
 var shapeColor = document.getElementById('color-choice');
+var eraseSquares = document.getElementById('eraseSquares');
+var eraseCircles = document.getElementById('eraseCircles');
+var squareArray = [];
+var circleArray = [];
 
 function Shape(x,y,w,color) {
   this.x = x;
@@ -22,6 +26,11 @@ Square.prototype.draw = function () {
   ctx.fillRect(this.x-this.w/2, this.y-this.w/2, this.w, this.w);
 };
 
+Square.prototype.erase = function () {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(this.x-this.w/2, this.y-this.w/2, this.w, this.w);
+};
+
 function Circle(x, y, w, color) {
   Shape.call(this, x, y, w, color);
 }
@@ -35,22 +44,42 @@ Circle.prototype.draw = function () {
   ctx.fill();
 };
 
+Circle.prototype.erase = function () {
+  ctx.beginPath();
+  ctx.fillStyle = 'white';
+  ctx.arc(this.x, this.y, this.w/2, 0, Math.PI*2, false);
+  ctx.fill();
+};
+
 function onClick(event) {
   var x = event.clientX - canvas.offsetLeft;
   var y = event.clientY - canvas.offsetTop;
-  var w = shapeWidth.value;
+  var w = shapeWidth.value || 100;
   var color = shapeColor.value;
   if (shapeChoice.value === 'square') {
     var square = new Square(x,y,w, color);
     square.draw();
+    squareArray.push(square);
   } else {
     var circle = new Circle(x,y,w, color);
     circle.draw();
+    circleArray.push(circle);
   }
 }
 
 canvas.addEventListener('click', onClick);
 
+eraseSquares.addEventListener('click', function () {
+  squareArray.forEach(function (square) {
+    square.erase();
+  });
+});
+
+eraseCircles.addEventListener('click', function () {
+  circleArray.forEach(function (circle) {
+    circle.erase();
+  });
+});
 
 //draw triangle
 // ctx.fillStyle = 'yellow';
